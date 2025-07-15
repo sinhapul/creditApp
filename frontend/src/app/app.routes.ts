e59301fc01464e7ff5b718bc5522e6ac35d7b534
may-ank0813 <mayanksaini1913@gmail.com>
2025-07-15 23:57:22 +0530

    routes for application

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', loadComponent: () => import('./welcome/welcome.component').then(m => m.WelcomeComponent)
+    }
+];
