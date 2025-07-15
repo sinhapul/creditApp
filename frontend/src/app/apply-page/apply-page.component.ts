125d424943f2592df180ab618265edb6c5a32cdf
may-ank0813 <mayanksaini1913@gmail.com>
2025-07-15 23:57:54 +0530

    home page

---
+++ frontend/src/app/apply-page/apply-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-page',
+  templateUrl: './apply-page.component.html',
+  styleUrls: ['./apply-page.component.css']
+})
+export class ApplyPageComponent {
+
+}
---
+++ frontend/src/app/home/components/eligibility/eligibility.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eligibility',
+  standalone: true,
+  templateUrl: './eligibility.component.html',
+  styleUrls: ['./eligibility.component.css']
+})
+export class EligibilityComponent {
+
+}
