af08fb340d501a9d0db04ac632c886caece185df
may-ank0813 <mayanksaini1913@gmail.com>
2025-07-24 23:11:49 +0530

    farmer image

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { ApplyFormComponent } from '../apply-form/apply-form.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BackendService } from '../backend.service';
 
 @Component({
   selector: 'app-content',
@@ -11,6 +12,12 @@ import { RouterModule } from '@angular/router';
   styleUrl: './content.component.scss'
 })
 export class ContentComponent {
+  user ='';
+  constructor(private backendService: BackendService, private cdr: ChangeDetectorRef) {
+    backendService.userId$.subscribe(res => {
+      this.user = res;
+    })
+  }
   showApplyForm = false;
   onApply() {
     this.showApplyForm = true;
