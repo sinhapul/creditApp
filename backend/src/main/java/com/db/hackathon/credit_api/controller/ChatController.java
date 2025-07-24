79ff800f0a89fefcb613d61a33052cca83e1dd7e
may-ank0813 <mayanksaini1913@gmail.com>
2025-07-24 16:07:42 +0530

    cors error

--- backend/src/main/java/com/db/hackathon/credit_api/controller/ChatController.java
+++ backend/src/main/java/com/db/hackathon/credit_api/controller/ChatController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/chat")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ChatController {
 
     private final ChatbotService chatbotService;
